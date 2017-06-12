f4cd2c5775c29afac075d48d0b34b3633fbb2fee
Serge Puchnin <Sergey.Puchnin@gmail.com>
2017-06-12 19:20:56 +0300

    Add Node to the head

--- src/main/java/com/company/LinkedList.java
+++ src/main/java/com/company/LinkedList.java
@@ -2,25 +2,29 @@ package com.company;
 
 /**
  * Let remember Linked List
- *
+ * <p>
  * Created by pss on 12.06.17.
  */
 public class LinkedList {
 
-    Node headNode;
-
+    private Node headNode;
 
     private class Node {
-
         String nodeData;
         int nodeID;
 
         Node nextNode;
 
-        public Node(String nodeData, int nodeID) {
+        Node(String nodeData, int nodeID) {
             this.nodeData = nodeData;
             this.nodeID = nodeID;
             this.nextNode = null;
         }
+    }
+
+    public void addNodeToTheHead(String nodeData, int nodeID) {
+        Node node = new Node(nodeData, nodeID);
+        node.nextNode = headNode;
+        headNode = node;
     }
 }
