3e26adf1021d35c1f2ddc18ef661d5eb1eb48fb1
Serge Puchnin <Sergey.Puchnin@gmail.com>
2017-06-13 20:38:40 +0300

    add constructors

--- src/main/java/com/company/BinaryHeap.java
+++ src/main/java/com/company/BinaryHeap.java
@@ -3,10 +3,14 @@ package com.company;
 /**
  * an implementation of Binary Heap on Array
  * need to try rewrite with generics
- *
+ * <p>
  * Created by pss on 13.06.17.
  */
 public class BinaryHeap {
+    final static int DEFAULT_CAPACITY = 16;
+    Node[] anArray;
+    int size;
+
     private class Node {
         String nodeData;
         int nodeID;
@@ -19,6 +23,19 @@ public class BinaryHeap {
         public String toMinimalString() {
             return "{" + "'" + nodeData + '\'' + ',' + nodeID + '}';
         }
+
+        public int getNodeID() {
+            return nodeID;
+        }
+    }
+
+    public BinaryHeap(int arraySize) {
+        anArray = new Node[arraySize];
+        size = 0;
+    }
+
+    public BinaryHeap() {
+        this(DEFAULT_CAPACITY);
     }
 
 
